16ddff1d923ddf0b92b7b3d64a74ac5e45959586
fedebacci <federicobacci1999@gmail.com>
2025-04-02 17:34:03 +0200

    implementazione iniziale dello switch (da finire con todo)

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,22 +4,22 @@
 // - per i multipli di 3 e di 5 stampi "FizzBuzz" al posto del numero
 
 // IMPOSTO IL NUMERO MINIMO E IL NUMERO MASSIMO COME COSTANTI IN MODO DA POTERLI MODIFICARE IN MODO PIÚ DINAMICA
-// const countFromNumber = 1;
-// const countToNumber = 100;
-// const dividerOne = 3;
-// const outputOne = "Fizz";
-// const dividerTwo = 5;
-// const outputTwo = "Buzz";
-// const counterStep = 1;
+const countFromNumber = 1;
+const countToNumber = 100;
+const dividerOne = 3;
+const outputOne = "Fizz";
+const dividerTwo = 5;
+const outputTwo = "Buzz";
+const counterStep = 1;
 
 // RICHIEDO SUL MOMENTO TUTTE LE INFORMAZIONI ALL'UTENTE, JUST FOR FUN
-const countFromNumber = parseInt(prompt("Da quale numero inizio a contare?", 1));
-const countToNumber = parseInt(prompt("A quale numero smetto di contare?", 100));
-const counterStep = parseInt(prompt("Ogni quanto incremento il contatore?", 1));
-const dividerOne = parseInt(prompt("Quale è il primo divisore?", 3));
-const outputOne = prompt("Cosa devo scrivere per i numeri divisibili per primo divisore?", "Fizz").toString();
-const dividerTwo = parseInt(prompt("Quale è il secondo divisore?", 5));
-const outputTwo = prompt("Cosa devo scrivere per i numeri divisibili per primo divisore?", "Buzz").toString();
+// const countFromNumber = parseInt(prompt("Da quale numero inizio a contare?", 1));
+// const countToNumber = parseInt(prompt("A quale numero smetto di contare?", 100));
+// const counterStep = parseInt(prompt("Ogni quanto incremento il contatore?", 1));
+// const dividerOne = parseInt(prompt("Quale è il primo divisore?", 3));
+// const outputOne = prompt("Cosa devo scrivere per i numeri divisibili per primo divisore?", "Fizz").toString();
+// const dividerTwo = parseInt(prompt("Quale è il secondo divisore?", 5));
+// const outputTwo = prompt("Cosa devo scrivere per i numeri divisibili per primo divisore?", "Buzz").toString();
 
 console.info("countFromNumber:", countFromNumber)
 console.info("countToNumber:", countToNumber)
@@ -65,16 +65,42 @@ if (isNaN(countFromNumber) || isNaN(countToNumber) || isNaN(counterStep) || isNa
 
 } else {
     for (let i = countFromNumber; i <= countToNumber; i += counterStep) {
+        
+        // SVOLGIMENTO ESERCIZIO 
         let output;
-        if (i % dividerOne == 0 && i % dividerTwo == 0) {
-            output = `${outputOne}${outputTwo} (${i})`
-        } else if (i % dividerOne == 0) {
-            output = `${outputOne} (${i})`
-        } else if (i % dividerTwo == 0) {
-            output = `${outputTwo} (${i})`
-        } else {
-            output = i
+        // if (i % dividerOne == 0 && i % dividerTwo == 0) {
+        //     output = `${outputOne}${outputTwo} (${i})`
+        // } else if (i % dividerOne == 0) {
+        //     output = `${outputOne} (${i})`
+        // } else if (i % dividerTwo == 0) {
+        //     output = `${outputTwo} (${i})`
+        // } else {
+        //     output = i
+        // }
+        // console.log(output)
+        // console.log("______________________________________")
+
+
+        // TEST SWITCH
+        // @ TODO - MIGLIORARE CERCANDO UN MODO DI NON RIPETERE SEMPRE IL LOG DELL'OUTPUT (CON FALL-THROUGH, INVERTENDO ORDINE DEI CONTROLLI DI DIVISIBILITÁ? EVENTUALMENTE NECESSARIO AGGIUNGERE CONTROLLI NEGATIVI COME ALL'INIZIO (NON CREDO)?)
+        switch (true) {
+            case i % dividerOne == 0 && i % dividerTwo == 0:
+                output = `${outputOne}${outputTwo} (${i})`;
+                console.log(output);
+                break;
+            case i % dividerOne == 0:
+                output = `${outputOne} (${i})`;
+                console.log(output);
+                break;
+            case i % dividerTwo == 0:
+                output = `${outputTwo} (${i})`;
+                console.log(output);
+                break;
+            default:
+                console.log(i);
+                break;
         }
-        console.log(output)
+        
+
     }
 }
