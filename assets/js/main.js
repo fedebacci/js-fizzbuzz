3c74b08da44cd136ac53a9485dc1a1984ae982cb
fedebacci <federicobacci1999@gmail.com>
2025-04-02 16:59:18 +0200

    ricezione dei valori da utente ed implementazione messaggi di errore

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,24 +4,77 @@
 // - per i multipli di 3 e di 5 stampi "FizzBuzz" al posto del numero
 
 // IMPOSTO IL NUMERO MINIMO E IL NUMERO MASSIMO COME COSTANTI IN MODO DA POTERLI MODIFICARE IN MODO PIÚ DINAMICA
-const countFromNumber = 1;
-const countToNumber = 100;
-const dividerOne = 3;
-const outputOne = "Fizz";
-const dividerTwo = 5;
-const outputTwo = "Buzz";
-const counterStep = 1;
+// const countFromNumber = 1;
+// const countToNumber = 100;
+// const dividerOne = 3;
+// const outputOne = "Fizz";
+// const dividerTwo = 5;
+// const outputTwo = "Buzz";
+// const counterStep = 1;
 
-for (let i = countFromNumber; i <= countToNumber; i += counterStep) {
-    let output;
-    if (i % dividerOne == 0 && i % dividerTwo == 0) {
-        output = `${outputOne}${outputTwo} (${i})`
-    } else if (i % dividerOne == 0) {
-        output = `${outputOne} (${i})`
-    } else if (i % dividerTwo == 0) {
-        output = `${outputTwo} (${i})`
-    } else {
-        output = i
+// RICHIEDO SUL MOMENTO TUTTE LE INFORMAZIONI ALL'UTENTE, JUST FOR FUN
+const countFromNumber = parseInt(prompt("Da quale numero inizio a contare?", 1));
+const countToNumber = parseInt(prompt("A quale numero smetto di contare?", 100));
+const counterStep = parseInt(prompt("Ogni quanto incremento il contatore?", 1));
+const dividerOne = parseInt(prompt("Quale è il primo divisore?", 3));
+const outputOne = prompt("Cosa devo scrivere per i numeri divisibili per primo divisore?", "Fizz").toString();
+const dividerTwo = parseInt(prompt("Quale è il secondo divisore?", 5));
+const outputTwo = prompt("Cosa devo scrivere per i numeri divisibili per primo divisore?", "Buzz").toString();
+
+console.info("countFromNumber:", countFromNumber)
+console.info("countToNumber:", countToNumber)
+console.info("counterStep:", counterStep)
+console.info("dividerOne:", dividerOne)
+console.info("outputOne:", outputOne)
+console.info("dividerTwo:", dividerTwo)
+console.info("outputTwo:", outputTwo)
+
+if (isNaN(countFromNumber) || isNaN(countToNumber) || isNaN(counterStep) || isNaN(dividerOne) || outputOne.length == 0 || !isNaN(parseInt(outputOne)) || isNaN(dividerTwo) || outputTwo.length == 0 || !isNaN(parseInt(outputTwo))) {
+
+    let errorMessage = "C'è stato un errore, leggi le istruzioni sotto riportate e ricarica la pagina per riprovare."
+
+    if (isNaN(countFromNumber)) {
+        errorMessage = `${errorMessage} \n~ IL NUMERO DA CUI PARTIRE DEVE ESSERE ESPRESSO SOTTO FORMA DI NUMERO!`
     }
-    console.log(output)
+    if (isNaN(countToNumber)) {
+        errorMessage = `${errorMessage} \n~ IL NUMERO FINO A CUI CONTARE DEVE ESSERE ESPRESSO SOTTO FORMA DI NUMERO!`
+    }
+    if (isNaN(counterStep)) {
+        errorMessage = `${errorMessage} \n~ IL NUMERO PER INCREMENTARE IL CONTATORE DEVE ESSERE ESPRESSO SOTTO FORMA DI NUMERO!`
+    }
+    if (isNaN(dividerOne)) {
+        errorMessage = `${errorMessage} \n~ IL PRIMO DIVISORE DEVE ESSERE ESPRESSO SOTTO FORMA DI NUMERO!`
+    }
+    if (outputOne.length == 0) {
+        errorMessage = `${errorMessage} \n~ IL TESTO DEL PRIMO DIVISORE NON PUO ESSERE VUOTO!`
+    }
+    if (!isNaN(parseInt(outputOne))) {
+        errorMessage = `${errorMessage} \n~ IL TESTO DEL PRIMO DIVISORE NON PUO ESSERE ESPRESSO SOTTO FORMA DI NUMERO!`
+    }
+    if (isNaN(dividerTwo)) {
+        errorMessage = `${errorMessage} \n~ IL SECONDO DIVISORE DEVE ESSERE ESPRESSO SOTTO FORMA DI NUMERO!`
+    }
+    if (outputTwo.length == 0) {
+        errorMessage = `${errorMessage} \n~ IL TESTO DEL SECONDO DIVISORE NON PUO ESSERE VUOTO!`
+    }
+    if (!isNaN(parseInt(outputTwo))) {
+        errorMessage = `${errorMessage} \n~ IL TESTO DEL SECONDO DIVISORE NON PUO ESSERE ESPRESSO SOTTO FORMA DI NUMERO!`
+    }
+
+    alert(errorMessage)
+
+} else {
+    for (let i = countFromNumber; i <= countToNumber; i += counterStep) {
+        let output;
+        if (i % dividerOne == 0 && i % dividerTwo == 0) {
+            output = `${outputOne}${outputTwo} (${i})`
+        } else if (i % dividerOne == 0) {
+            output = `${outputOne} (${i})`
+        } else if (i % dividerTwo == 0) {
+            output = `${outputTwo} (${i})`
+        } else {
+            output = i
+        }
+        console.log(output)
+    }
 }
