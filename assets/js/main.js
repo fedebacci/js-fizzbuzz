144c9ee6de41f56b33335983393c8b9cfcb50e99
fedebacci <federicobacci1999@gmail.com>
2025-04-02 14:02:30 +0200

    svolgimento base esercizio

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,21 @@
 // - per i multipli di 3 stampi "Fizz" al posto del numero
 // - per i multipli di 5 stampi "Buzz" al posto del numero
 // - per i multipli di 3 e di 5 stampi "FizzBuzz" al posto del numero
+
+// IMPOSTO IL NUMERO MINIMO E IL NUMERO MASSIMO COME COSTANTI IN MODO DA POTERLI MODIFICARE IN MODO PIÚ DINAMICA
+const countFromNumber = 1;
+const countToNumber = 100;
+
+let output;
+for (let i = countFromNumber; i <= countToNumber; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        output = 'FizzBuzz'
+    } else if (i % 3 == 0 && i % 5 != 0) {
+        output = 'Fizz'
+    } else if (i % 3 != 0 && i % 5 == 0) {
+        output = 'Buzz'
+    } else {
+        output = i
+    }
+    console.log(output)
+}
