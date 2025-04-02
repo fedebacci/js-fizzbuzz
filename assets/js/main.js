0655cb9ac26fee4fe76ee30a2ce0227cfbf7e810
fedebacci <federicobacci1999@gmail.com>
2025-04-02 16:25:07 +0200

    implementazione correzioni donato e aggiunta costanti per maggiore modularità

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,15 +6,20 @@
 // IMPOSTO IL NUMERO MINIMO E IL NUMERO MASSIMO COME COSTANTI IN MODO DA POTERLI MODIFICARE IN MODO PIÚ DINAMICA
 const countFromNumber = 1;
 const countToNumber = 100;
+const dividerOne = 3;
+const outputOne = "Fizz";
+const dividerTwo = 5;
+const outputTwo = "Buzz";
+const counterStep = 1;
 
-let output;
-for (let i = countFromNumber; i <= countToNumber; i++) {
-    if (i % 3 == 0 && i % 5 == 0) {
-        output = `FizzBuzz (${i})`
-    } else if (i % 3 == 0 && i % 5 != 0) {
-        output = `Fizz (${i})`
-    } else if (i % 3 != 0 && i % 5 == 0) {
-        output = `Buzz (${i})`
+for (let i = countFromNumber; i <= countToNumber; i += counterStep) {
+    let output;
+    if (i % dividerOne == 0 && i % dividerTwo == 0) {
+        output = `${outputOne}${outputTwo} (${i})`
+    } else if (i % dividerOne == 0) {
+        output = `${outputOne} (${i})`
+    } else if (i % dividerTwo == 0) {
+        output = `${outputTwo} (${i})`
     } else {
         output = i
     }
