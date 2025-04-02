96f2733e8b3091333ffbb9252f82f6301cb22fb3
fedebacci <federicobacci1999@gmail.com>
2025-04-02 13:53:58 +0200

    preparazione esercizio

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+// Scrivi un programma che stampi i numeri da 1 a 100,
+// - per i multipli di 3 stampi "Fizz" al posto del numero
+// - per i multipli di 5 stampi "Buzz" al posto del numero
+// - per i multipli di 3 e di 5 stampi "FizzBuzz" al posto del numero
